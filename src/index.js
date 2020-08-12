786a8d802fdd5deedfbd3d6035702c3bc7a780ad
Diego Rojas <diegorojas431@gmail.com>
2020-08-12 09:32:02 -0500

    Estructura del Header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import HelloWorld from './components/HelloWorld.jsx'
+import App from './containers/App'
 
-const App = document.getElementById('app')
+const Container = document.getElementById('app')
 
-ReactDOM.render( <HelloWorld/> , App)
+ReactDOM.render( <App/> , Container)
